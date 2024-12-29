04daa210ce8bdaf3ae27ad5f74192cadcd7fca27
Michael-24-Ze <gucci930429@gmail.com>
2024-12-30 02:06:34 +0800

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 a = int(input("enter a:"))
 b = int(input("enter b:"))
-if a > b:
-    print("%d is bigger than %d"%(a,b))
-else:
+if a < b:
     print("%d is smaller than %d"%(a,b))
+else:
+    print("%d is bigger than %d"%(a,b))
 print(a,b)
